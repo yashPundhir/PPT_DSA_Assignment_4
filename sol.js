41a7fdc6cfe764c654620014234d754d000d130b
yashPundhir <yash.pundhir.prof@gmail.com>
2023-06-03 16:48:08 +0530

    Ques 4 solved

--- sol.js
+++ sol.js
@@ -80,6 +80,71 @@ function commonElements(arr1, arr2, arr3) {
 
 /*
 
+**Question 3**
+
+Given a 2D integer array matrix, return *the **transpose** of* matrix.
+
+The **transpose** of a matrix is the matrix flipped over its main diagonal, switching the matrix's row and column indices.
+
+**Example 1:**
+
+Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+
+Output: [[1,4,7],[2,5,8],[3,6,9]]
+
+*/
+
+// Solution 3
+
+function transposeMatrix(matrix) {
+	let temp = 0;
+	for (let i = 0; i < matrix.length; i++) {
+		for (let j = 0; j < matrix[i].length; j++) {
+			temp = matrix[i][j];
+			matrix[i][j] = matrix[j][i];
+			matrix[j][i] = temp;
+		}
+	}
+	return matrix;
+}
+
+/*
+
+**Question 4**
+
+Given an integer array nums of 2n integers, group these integers into n pairs (a1, b1), (a2, b2), ..., (an, bn) such that the sum of min(ai, bi) for all i is **maximized**. Return *the maximized sum*.
+
+**Example 1:**
+
+Input: nums = [1,4,3,2]
+
+Output: 4
+
+**Explanation:** All possible pairings (ignoring the ordering of elements) are:
+
+1. (1, 4), (2, 3) -> min(1, 4) + min(2, 3) = 1 + 2 = 3
+
+2. (1, 3), (2, 4) -> min(1, 3) + min(2, 4) = 1 + 2 = 3
+
+3. (1, 2), (3, 4) -> min(1, 2) + min(3, 4) = 1 + 3 = 4
+
+So the maximum possible sum is 4.
+
+*/
+
+// Solution 4
+
+function maxPairSum(nums) {
+	nums.sort((a, b) => a - b);
+	let sum = 0;
+	for (let index = 0; index < nums.length; index = index + 2) {
+		sum = sum + nums[index];
+	}
+	return sum;
+}
+
+/*
+
 **Question 8**
 
 Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn].
@@ -95,3 +160,12 @@ Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,.
 **Explanation:** Since x1=2, x2=5, x3=1, y1=3, y2=4, y3=7 then the answer is [2,3,5,4,1,7].
 
 */
+
+// Solution 8
+
+// function pairArray(nums){
+//   const n = nums.length/2;
+//   for (let i = 0; i < n; i++) {
+
+//   }
+// }
