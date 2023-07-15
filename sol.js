7e59e262f832b810a5a6d94e46cb04724d1ad6ec
yashPundhir <yash.pundhir.prof@gmail.com>
2023-07-15 19:27:48 +0530

    All ques done

--- sol.js
+++ sol.js
@@ -80,6 +80,67 @@ function commonElements(arr1, arr2, arr3) {
 
 /*
 
+**Question 2**
+
+Given two **0-indexed** integer arrays nums1 and nums2, return *a list* answer *of size* 2 *where:*
+
+- answer[0] *is a list of all **distinct** integers in* nums1 *which are **not** present in* nums2*.*
+- answer[1] *is a list of all **distinct** integers in* nums2 *which are **not** present in* nums1.
+
+**Note** that the integers in the lists may be returned in **any** order.
+
+**Example 1:**
+
+**Input:** nums1 = [1,2,3], nums2 = [2,4,6]
+
+**Output:** [[1,3],[4,6]]
+
+**Explanation:**
+
+For nums1, nums1[1] = 2 is present at index 0 of nums2, whereas nums1[0] = 1 and nums1[2] = 3 are not present in nums2. Therefore, answer[0] = [1,3].
+
+For nums2, nums2[0] = 2 is present at index 1 of nums1, whereas nums2[1] = 4 and nums2[2] = 6 are not present in nums2. Therefore, answer[1] = [4,6].
+
+*/
+
+// Solution
+
+function findMissingDistinct(nums1, nums2) {
+	const set1 = new Set();
+	const set2 = new Set();
+
+	for (let num of nums1) {
+		set1.add(num);
+	}
+
+	for (let num of nums2) {
+		set2.add(num);
+	}
+
+	const notInNums2 = [];
+	const notInNums1 = [];
+
+	for (let num of nums1) {
+		if (!set2.has(num)) {
+			notInNums2.push(num);
+		}
+	}
+
+	for (let num of nums2) {
+		if (!set1.has(num)) {
+			notInNums1.push(num);
+		}
+	}
+
+	return [notInNums1, notInNums2];
+}
+
+const nums1 = [1, 2, 3];
+const nums2 = [2, 4, 6];
+console.log(findMissingDistinct(nums1, nums2)); // Output: [[1,3],[4,6]]
+
+/*
+
 **Question 3**
 
 Given a 2D integer array matrix, return *the **transpose** of* matrix.
@@ -145,6 +206,50 @@ function maxPairSum(nums) {
 
 /*
 
+**Question 5**
+You have n coins and you want to build a staircase with these coins. The staircase consists of k rows where the ith row has exactly i coins. The last row of the staircase **may be** incomplete.
+
+Given the integer n, return *the number of **complete rows** of the staircase you will build*.
+
+**Example 1:**
+
+[]()
+
+![v2.jpg](https://s3-us-west-2.amazonaws.com/secure.notion-static.com/4bd91cfa-d2b1-47b3-8197-a72e8dcfff4b/v2.jpg)
+
+**Input:** n = 5
+
+**Output:** 2
+
+**Explanation:** Because the 3rd row is incomplete, we return 2.
+
+*/
+
+// Solution
+
+function arrangeCoins(n) {
+	let left = 1;
+	let right = n;
+
+	while (left <= right) {
+		const mid = Math.floor((left + right) / 2);
+		const sum = (mid * (mid + 1)) / 2;
+
+		if (sum <= n) {
+			left = mid + 1;
+		} else {
+			right = mid - 1;
+		}
+	}
+
+	return right;
+}
+
+const n = 5;
+console.log(arrangeCoins(n)); // Output: 2
+
+/*
+
 **Question 6**
 
 Given an integer array nums sorted in **non-decreasing** order, return *an array of **the squares of each number** sorted in non-decreasing order*.
@@ -179,6 +284,48 @@ function square(nums) {
 
 /*
 
+**Question 7**
+You are given an m x n matrix M initialized with all 0's and an array of operations ops, where ops[i] = [ai, bi] means M[x][y] should be incremented by one for all 0 <= x < ai and 0 <= y < bi.
+
+Count and return *the number of maximum integers in the matrix after performing all the operations*
+
+**Example 1:**
+
+![q4.jpg](https://s3-us-west-2.amazonaws.com/secure.notion-static.com/4d0890d0-7bc7-4f59-be8e-352d9f3c1c52/q4.jpg)
+
+**Input:** m = 3, n = 3, ops = [[2,2],[3,3]]
+
+**Output:** 4
+
+**Explanation:** The maximum integer in M is 2, and there are four of it in M. So return 4.
+
+*/
+
+// Solution
+
+function maxCount(m, n, ops) {
+	let minRow = m;
+	let minCol = n;
+
+	for (let [ai, bi] of ops) {
+		minRow = Math.min(minRow, ai);
+		minCol = Math.min(minCol, bi);
+	}
+
+	return minRow * minCol;
+}
+
+// Example usage:
+const m = 3;
+const n = 3;
+const ops = [
+	[2, 2],
+	[3, 3],
+];
+console.log(maxCount(m, n, ops)); // Output: 4
+
+/*
+
 **Question 8**
 
 Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn].
@@ -197,9 +344,22 @@ Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,.
 
 // Solution 8
 
-// function pairArray(nums){
-//   const n = nums.length/2;
-//   for (let i = 0; i < n; i++) {
+function rearrangeArray(nums, n) {
+	const result = [];
 
-//   }
-// }
+	let ptrX = 0;
+	let ptrY = n;
+
+	for (let i = 0; i < n; i++) {
+		result.push(nums[ptrX]);
+		result.push(nums[ptrY]);
+		ptrX++;
+		ptrY++;
+	}
+
+	return result;
+}
+
+const nums = [2, 5, 1, 3, 4, 7];
+const n = 3;
+console.log(rearrangeArray(nums, n)); // Output: [2, 3, 5, 4, 1, 7]
